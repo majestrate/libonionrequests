e78a2a13e596016139f6afb4a5f478a6bbcdd989
Jeff Becker <jeff@i2p.rocks>
2021-10-14 16:36:07 -0400

    add header

---
+++ onionreq/onionreq.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+#include "onion.hpp"
+#include "consensus.hpp"
+#include "transport.hpp"
+#include "onion_maker.hpp"
+#include "snode_info.hpp"
+#include "sogs_info.hpp"
+#include "key_types.hpp"
